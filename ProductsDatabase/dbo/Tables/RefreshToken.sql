4ec0bd20a45cbae9c302f3adc55be5b4528ba157
psolkar <parvejsolkar1@gmail.com>
2025-03-27 16:08:17 +0530

    Removed unncessary folders

--- ProductsDatabase/dbo/Tables/RefreshToken.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[RefreshToken] (
-    [Id]       INT            NOT NULL,
-    [UserId]   NVARCHAR (50)  NOT NULL,
-    [Token]    NVARCHAR (MAX) NOT NULL,
-    [IsActive] BIT            NOT NULL,
-    CONSTRAINT [PK_RefreshToken] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_RefreshToken_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
-);
-
--- ProductsDatabase/dbo/Tables/User.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[User] (
-    [UserId]   NVARCHAR (50)  NOT NULL,
-    [Password] NVARCHAR (MAX) NOT NULL,
-    [Role]     NVARCHAR (50)  NOT NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC),
-    CONSTRAINT [FK_User_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
-);
-
